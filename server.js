ef106dd5d815a1afe1391014ac0358e0e12c7097
oprty131 <slwmplylw@gmail.com>
2026-04-21 20:09:51 +0300

    Update server.js

--- server.js
+++ server.js
@@ -5,48 +5,75 @@ const io = require('socket.io')(http);
 
 app.use(express.static('public'));
 
+// Store active rooms and their players
+const rooms = {};
+
 io.on('connection', (socket) => {
     
-    socket.on('joinRoom', ({ username, room }) => {
-        // Check how many people are already in this specific room
+    socket.on('joinRoom', ({ username, room, character }) => {
         const clients = io.sockets.adapter.rooms.get(room);
         const numClients = clients ? clients.size : 0;
 
-        if (numClients >= 2) {
-            socket.emit('roomError', 'ROOM IS FULL');
+        if (numClients >= 14) {
+            socket.emit('roomError', 'ARENA IS FULL (14/14)');
             return;
         }
 
-        // Join the room and assign player 1 or 2
         socket.join(room);
-        const playerNum = numClients === 0 ? 1 : 2;
-        socket.data = { username, room, playerNum };
-        
-        console.log(`${username} joined room ${room} as Player ${playerNum}`);
-        
-        // Tell the player who they are
-        socket.emit('init', playerNum);
+        socket.data = { username, room, id: socket.id };
 
-        // If player 2 joins, tell both players the game can start
-        if (playerNum === 2) {
-            io.to(room).emit('gameReady');
-        }
+        if (!rooms[room]) rooms[room] = { players: {} };
+        
+        // Register the new player
+        rooms[room].players[socket.id] = {
+            id: socket.id,
+            username: username.substring(0, 10),
+            character: character,
+            score: 0
+        };
+        
+        console.log(`${username} joined arena ${room} as ${character}`);
+        
+        // Send the joining player their ID and current room state
+        socket.emit('init', { id: socket.id, players: rooms[room].players });
+        
+        // Tell everyone else someone new arrived
+        socket.to(room).emit('playerJoined', rooms[room].players[socket.id]);
 
-        // Bounce data ONLY to the other person in the same room
+        // Broadcast movements
         socket.on('move', (data) => {
-            socket.to(room).emit('opponentMoved', data);
+            socket.to(room).emit('opponentMoved', { id: socket.id, ...data });
         });
 
-        socket.on('shoot', (data) => {
-            socket.to(room).emit('opponentShot', data);
+        // Broadcast attacks/actions
+        socket.on('action', (data) => {
+            socket.to(room).emit('opponentAction', { id: socket.id, ...data });
+        });
+
+        // Handle kills for the leaderboard
+        socket.on('scoredKill', (killerId) => {
+            if (rooms[room] && rooms[room].players[killerId]) {
+                rooms[room].players[killerId].score += 1;
+                io.to(room).emit('updateLeaderboard', rooms[room].players);
+            }
         });
     });
 
     socket.on('disconnect', () => {
         if (socket.data && socket.data.room) {
-            console.log(`${socket.data.username} left room ${socket.data.room}`);
-            // Tell the other player their opponent left
-            socket.to(socket.data.room).emit('opponentLeft');
+            console.log(`${socket.data.username} left arena ${socket.data.room}`);
+            const room = socket.data.room;
+            
+            if (rooms[room]) {
+                delete rooms[room].players[socket.id];
+                io.to(room).emit('playerLeft', socket.id);
+                io.to(room).emit('updateLeaderboard', rooms[room].players);
+                
+                // Cleanup empty rooms
+                if (Object.keys(rooms[room].players).length === 0) {
+                    delete rooms[room];
+                }
+            }
         }
     });
 });
